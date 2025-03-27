b2019492baa69b0629eca002d1e7dafa974fd619
pgcr007 <prathameshghawarepg@gmail.com>
2025-03-27 23:11:18 +0530

    inverted triangle

--- traingle.js
+++ traingle.js
@@ -1,7 +1,18 @@
-let n = 6;
-ptrn ="";
+// let n = 6;
 
-for(let i = 1 ; i<= n;i++)
+// for(let i = 0 ; i <= n;i++)
+// {
+//     console.log("*".repeat(i));
+// }
+
+
+// triangle 
+
+let  m = 8;
+
+for( let i = m ; i >= 1;i--)
 {
-    console.log("*".repeat(i));
+    let spaces = " ".repeat(m-i);
+    let starts = "* ".repeat(i);
+    console.log(spaces+starts);
 }
